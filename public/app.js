a9527123dacfe7e7f0939554dd8bd8fdd1952b80
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2022-12-31 17:12:35 +0300

    feat: 18 Динамическое изменение корзины

--- public/app.js
+++ public/app.js
@@ -1,10 +1,19 @@
+const toCurrency = (price) => {
+  return new Intl.NumberFormat('ru-RU', {
+    currency: 'rub',
+    style: 'currency',
+  }).format(price);
+};
+
 // будем менять значение цены
 document.querySelectorAll('.price').forEach((node) => {
   // форматировать будем с помощью Intl.NumberFormat
-  node.textContent = new Intl.NumberFormat('ru-RU', {
-    currency: 'rub',
-    style: 'currency',
-  }).format(node.textContent);
+  // вынесли в отдельную ф-цию в уроке 18
+  // node.textContent = new Intl.NumberFormat('ru-RU', {
+  //   currency: 'rub',
+  //   style: 'currency',
+  // }).format(node.textContent);
+  node.textContent = toCurrency(node.textContent);
 });
 
 const $card = document.querySelector('#card');
@@ -23,7 +32,32 @@ if ($card) {
       })
         .then((res) => res.json())
         .then((card) => {
-          console.log(card);
+          if (card.courses.length) {
+            //обновляем таблицу (обратимся и пересчитаем)
+            const html = card.courses
+              .map((c) => {
+                return `
+              <tr>
+            <td>${c.title}</td>
+              <td>${c.count}</td>
+              <td>
+                <button
+                  class='btn btn-small js-remove'
+                  data-id='${c.id}'
+                >Delete</button>
+              </td>
+            </tr>
+              `;
+              })
+              .join('');
+            $card.querySelector('tbody').innerHTML = html;
+
+            //пересчет цены
+            $card.querySelector('.price').textContent = toCurrency(card.price);
+          } else {
+            // иначе очищаем
+            $card.innerHTML = `<p>Card is empty</p>`;
+          }
         });
     }
   });
