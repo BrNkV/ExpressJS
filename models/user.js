5bd2a50c2d4415e23e061d707cc1430cfb77dab5
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2023-03-07 17:57:04 +0300

    feat: Добавление товара в корзину

--- models/user.js
+++ models/user.js
@@ -24,6 +24,39 @@ const userSchema = new Schema({
   },
 });
 
+// можем определить метод для вынесения логики (обязательное использование слова function т.к. работает с this)
+userSchema.methods.addToCart = function (course) {
+  // логика добавления курса в корзину
+  // в случае если курс уже в корзине, то необходимо увеличивать кол-во (поле count)
+  // чтобы не мутировать массив обратимся к нему через this и склонируем при помощи concat или спред оператором
+  const clonedItems = [...this.cart.items]; //copy
+  // найдем в массиве данный курс который нам необходим
+  const idx = clonedItems.findIndex((c) => {
+    // Для сравнивания необходим перевод объекта в строку т.к. тип Schema.Types.ObjectId
+    // сравниваем с переданным курсом
+    return c.courseId.toString() === course._id.toString();
+  });
+  // если курс нашелся увеличим кол-во, если курса нет, добавим в корзину
+  // в корзине уже есть такой курс, увеличим кол-во
+  if (idx >= 0) {
+    clonedItems[idx].count = clonedItems[idx].count + 1;
+  } // нет в корзине
+  else {
+    clonedItems.push({
+      courseId: course._id,
+      count: 1,
+    });
+  }
+
+  // написано для понятности в подробном виде
+  // this.cart = {items: clonedItems}
+  const newCart = { items: clonedItems };
+  this.cart = newCart;
+
+  // необходимо вернуть метод сохранения
+  return this.save();
+};
+
 // регистрируем модель 'User' с 'userSchema'
 module.exports = model('User', userSchema);
 
--- routes/card.js
+++ routes/card.js
@@ -3,16 +3,21 @@ const { Router } = require('express');
 //создадим роутер
 const router = Router();
 // подключим модель корзины и курсов
-const Card = require('../models/card');
+// const Card = require('../models/card'); - модель удалена
 const Course = require('../models/course');
 
 //добавим пост запрос
 // метод /add мы принимаем в объекте req.body.id
 router.post('/add', async (req, res) => {
   // для начала необходимо получить объект req.body.id
-  const course = await Course.getById(req.body.id);
+  // const course = await Course.getById(req.body.id);
+  const course = await Course.findById(req.body.id);
   // теперь необходимо обратиться к модели корзины и вызвать метод add в который передадим курс
-  await Card.add(course);
+  // await Card.add(course); - удаляем / будем работать с пользователем
+
+  //вызываем новый метод добавления в корзину
+  await req.user.addToCart(course);
+
   //   далее редирект на страницу корзины
   res.redirect('/card');
 });
@@ -28,14 +33,15 @@ router.delete('/remove/:id', async (req, res) => {
 // добавим обработчик метода get
 router.get('/', async (req, res) => {
   // получаем объект Card
-  const card = await Card.fetch();
+  // const card = await Card.fetch();
   // вызываем рендер
-  res.render('card', {
-    title: 'Cart',
-    isCard: true,
-    courses: card.courses,
-    price: card.price,
-  });
+  // res.render('card', {
+  //   title: 'Cart',
+  //   isCard: true,
+  //   courses: card.courses,
+  //   price: card.price,
+  // });
+  res.json({test: true}); //временно
 });
 
 module.exports = router;
