43eb4d4509e535cc80b02ab3ab3420f09a489a19
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2023-11-10 17:20:45 +0300

    feat: add example keys

---
+++ keys/example.index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  MONGODB_URI: '',
+  SESSION_SECRET: 'some secret value',
+  NOTISEND_API_KEY: '',
+  NOTISEND_LOG: '',
+  NOTISEND_PASS: '',
+  EMAIL_FROM: '',
+  BASE_URL: '',
+};
