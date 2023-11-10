e14b1455056214c32d87d20121bd9be9da70e6d8
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2023-11-10 17:14:27 +0300

    feat: del keys index

--- keys/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB_URI: 'mongodb://localhost:27017/shop',
-  SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '6dqmcjd1ajjaksk5otgu4s8bw4d5desi34gbyq6o', //1500 бесплатно
-};
