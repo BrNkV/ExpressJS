d01e5f54d312093f689f799c1dcbf949c73d05a7
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2024-02-11 19:15:01 +0300

    feat: add profile page

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const ordersRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile');
 
 // const User = require('./models/user');
 const varMiddleware = require('./middleware/variables');
@@ -126,6 +127,7 @@ app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 const PORT = process.env.PORT || 3000;
 
